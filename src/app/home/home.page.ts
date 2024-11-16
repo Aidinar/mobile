1af75ce24a54b7e9ef532e9abdf790dde2f524a0
Your Name <you@example.com>
2024-11-16 18:39:25 +0300

    test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  gg = "888-888-8888"
 
+  ww = "777"
+
+  numbers = ["gg", "ww"]
+  books = [
+    {q:"Место",n:"Москва"},
+    {q:"Язык",n:"Russian"}
+  ]
+  
   constructor() {}
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 
 }
