9cefa9d3670eb458d7c0ffd9b8948fb465931a62
Your Name <you@example.com>
2024-11-16 15:22:13 +0300

    one

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aid',
+  webDir: 'www'
+};
+
+export default config;
